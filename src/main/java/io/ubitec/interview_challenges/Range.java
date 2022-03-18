229310d3575ef14709180b98697675cae611046f
thai <giacatluong20121996@gmail.com>
2022-03-18 15:22:31 +0700

    Finish Level 1

--- src/main/java/io/ubitec/interview_challenges/Range.java
+++ src/main/java/io/ubitec/interview_challenges/Range.java
@@ -2,20 +2,27 @@ package io.ubitec.interview_challenges;
 
 public class Range {
 
+  private final int lowerbound;
+
+  private final int upperbound;
+
   /**
    * Constructor is private BY DESIGN.
    *
-   * TODO: Change the constructor to meet your requirements.
    */
-  private Range() {
+  private Range(int lowerbound, int upperbound) {
+    if (lowerbound > upperbound) {
+      throw new IllegalArgumentException("It is not allowed to create a Range with lowerbound > upperbound");
+    }
+    this.lowerbound = lowerbound;
+    this.upperbound = upperbound;
   }
 
   /**
    * Creates a new <b>closed</b> {@code Range} that includes both bounds.
    */
   public static Range of(int lowerbound, int upperbound) {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return new Range(lowerbound, upperbound);
   }
 
   /**
@@ -23,24 +30,21 @@ public class Range {
    * {@code Range}.
    */
   public boolean contains(int value) {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return lowerbound <= value && value <= upperbound;
   }
 
   /**
    * Returns the {@code lowerbound} of this {@code Range}.
    */
   public int lowerbound() {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return lowerbound;
   }
 
   /**
    * Returns the {@code upperbound} of this {@code Range}.
    */
   public int upperbound() {
-    // TODO: Implement here!
-    throw new RuntimeException("not implemented yet");
+    return upperbound;
   }
 
 }
